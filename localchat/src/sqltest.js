8d39b3211ffafce86a7ffae08b41d9c910fe2c3f
Jake Vosloo <jake.vosloo@outlook.com>
2024-07-21 19:49:59 +1000

    Create passwordlesss sql test file

--- localchat/src/sqltest.js
+++ localchat/src/sqltest.js
@@ -1,19 +1,20 @@
 const sql = require('mssql');
 
 const config = {
-    user: 'username', // better stored in an app setting such as process.env.DB_USER
-    password: 'password', // better stored in an app setting such as process.env.DB_PASSWORD
-    server: 'your_server.database.windows.net', // better stored in an app setting such as process.env.DB_SERVER
-    port: 1433, // optional, defaults to 1433, better stored in an app setting such as process.env.DB_PORT
-    database: 'AdventureWorksLT', // better stored in an app setting such as process.env.DB_NAME
+    server: 'zmsql1.database.windows.net',
+    port: 1433, 
+    database: 'ZenmeChat',
     authentication: {
-        type: 'default'
+        type: 'azure-active-directory-default'
     },
     options: {
         encrypt: true
     }
 }
 
+//https://learn.microsoft.com/en-us/azure/azure-sql/database/azure-sql-passwordless-migration-nodejs?view=azuresql&tabs=sign-in-azure-cli%2Cazure-portal-create%2Cazure-portal-assign%2Capp-service-identity
+//https://learn.microsoft.com/en-us/azure/azure-sql/database/azure-sql-javascript-mssql-quickstart?view=azuresql&tabs=passwordless%2Cservice-connector%2Cportal
+ 
 /*
     //Use Azure VM Managed Identity to connect to the SQL database
     const config = {
