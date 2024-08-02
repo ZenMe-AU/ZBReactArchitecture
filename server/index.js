e04b5b5b352361798ab3125e31b3815bad4618eb
Luke Yeh <lukeforwardto@gmail.com>
2024-08-02 15:21:20 +1000

    add param into the query

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from 'cors';
 import { sequelize } from './db.js';
 import { QueryTypes } from 'sequelize';
-import { Users } from "./models.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,9 +14,16 @@ const corsOptions = {
 // Use CORS middleware
 app.use(cors(corsOptions));
 app.get('/', async (req, res) => {
+  let device_id = 'l1';
+  let request_time = req.query.datetime || new Date().toISOString().slice(0, 16);
+  let interval = req.query.interval || 60;
+  let distance = (req.query.distance || 10) * 0.00001;
+  let limited = req.query.limited || 100;
 
-  let users = await getUsers();
-  res.send({'return': {'users':users}});
+  let start_time = new Date(new Date(request_time+'Z').getTime() - (interval * 60 * 1000)).toISOString().slice(0, 16);
+
+  let users = await getUsers(device_id, start_time,  request_time, distance, limited);
+  res.json({'return': {'users':users}});
 });
 
 app.listen(PORT, () => {
@@ -26,24 +32,25 @@ app.listen(PORT, () => {
 
 
 
-const getUsers = () => {
+const getUsers = (deviceId, from, to, distance, limited) => {
   let select_query = `
     SELECT   "users"."id", "location"."tid", "users"."name", "users"."avatar"
         FROM
         (SELECT "id", "topicId", "tid", "lat", "lon", "response", "createdAt" FROM "location" AS "location"
-                WHERE ("tid" = 'l1') AND ("createdAt" BETWEEN '2024-07-27 10:00' AND '2024-07-27 11:00')) AS "A",
+                WHERE ("tid" = $deviceId) AND ("createdAt" BETWEEN $from AND $to)) AS "A",
           "location" left JOIN "users" ON "location"."tid" = "users"."deviceId"
           WHERE
             ("location"."tid" != "A"."tid"
 
-                and ("location"."lat" BETWEEN "A"."lat"-0.00003 AND "A"."lat"+0.00003 )
-                and ("location"."lon" BETWEEN "A"."lon"-0.00003 AND "A"."lon"+0.00003 ))
+                and ("location"."lat" BETWEEN "A"."lat"-$distance AND "A"."lat"+$distance )
+                and ("location"."lon" BETWEEN "A"."lon"-$distance AND "A"."lon"+$distance ))
             and ("location"."createdAt" BETWEEN ("A"."createdAt" - INTERVAL '1 MINUTE') and ("A"."createdAt" + INTERVAL '1 MINUTE'))
 
         GROUP BY "users"."id", "location"."tid";
   `;
   try {
     return sequelize.query(select_query,{
+      bind: { deviceId:deviceId, from:from, to:to, distance:distance },
       type: QueryTypes.SELECT,
     });
   } catch (err) {
