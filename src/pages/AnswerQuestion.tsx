950776d86543f0436608c04709f1ec8c08979ac2
Luke Yeh <lukeforwardto@gmail.com>
2025-02-24 23:02:15 +0800

    feat(ui): change pie chart legend position & add word cloud

--- src/pages/AnswerQuestion.tsx
+++ src/pages/AnswerQuestion.tsx
@@ -21,6 +21,7 @@ import {
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
+import WordCloud from "react-d3-cloud";
 
 function AnswerQuestion() {
   const profileId = localStorage.getItem("profileId");
@@ -38,6 +39,7 @@ function AnswerQuestion() {
 
   const [pieChartData, setPieChartData] = useState([]);
   const [pieChartColors, setPieChartColors] = useState([]);
+  const [wordCloudData, setWordCloudData] = useState([]);
   const [isOption, setIsOption] = useState<boolean>(true);
   const navigate = useNavigate();
 
@@ -68,7 +70,7 @@ function AnswerQuestion() {
         const data = await getAnswerListByQuestionId(id);
         console.log(data);
         const answerList = data.reduce(
-          (acc, q) => {
+          (acc: { self: any[]; others: any[] }, q) => {
             if (q.profileId == profileId) {
               acc.self.push(q);
             } else {
@@ -106,62 +108,45 @@ function AnswerQuestion() {
   useEffect(() => {
     var categoryList = answerList;
     if (!isOption) {
-      console.log("openAI");
-      console.log(answerList);
-      // let list = answerList.map(({ id, answerText }) => ({
-      //   id,
-      //   text: answerText,
-      // }));
-      var categorizedData = [
-        { id: "f76725cf-f5af-4339-9201-a9db3216895a", category: "Mandarin" },
-        { id: "d520b3fb-f2ce-4d08-b865-6d813812b7c3", category: "Mandarin" },
-        { id: "d30f95fc-a02b-4e65-b256-751828367a17", category: "Uncertain" },
-        { id: "b6fd8cc5-486e-484e-a969-b5ef563eb75d", category: "Mandarin" },
-        { id: "6d265edd-e60e-4a67-b7e9-bb5512fc2246", category: "Mandarin" },
-        { id: "143c0d5d-9b2a-4226-95b1-50874a6ca9bb", category: "Taiwanese" },
-        { id: "a8b42d1d-c205-472e-95c7-43194db1769c", category: "Japanese" },
-        { id: "749590c9-8bbf-408a-98d0-09231bd2bda4", category: "Mandarin" },
-        { id: "0233ae13-be2e-49b1-a068-760583d2ed5c", category: "Mandarin" },
-        { id: "78bc5f31-ef0c-45e7-ad62-4f41844ba430", category: "Mandarin" },
-        { id: "08ade037-038e-4cdd-a525-d25caadb448a", category: "Uncertain" },
-        { id: "f94cb290-09f1-40b0-803e-3047abd3190b", category: "Mandarin" },
-        { id: "1e6f3973-1312-4c36-aec1-95f704e5a157", category: "Uncertain" },
-        { id: "a82b9556-d873-44ac-af69-6fd485dad1f4", category: "Mandarin" },
-        { id: "7a1cf68e-5d00-4553-9996-03aee87f70fd", category: "Hokkien" },
-        { id: "d60fce3d-ed42-49c5-83b4-ec547e4fb578", category: "Uncertain" },
-        { id: "f578d4ea-169b-4889-8f19-3a369fb65fc6", category: "Mandarin" },
-        { id: "4d27cd3a-9528-4bea-9685-20aa4fc4ac6e", category: "Mandarin" },
-        { id: "b18bc58a-30d2-4b06-9999-be5abbe085a2", category: "Mandarin" },
-      ];
-      categoryList = categorizedData.map(({ category }) => ({
-        optionId: category,
+      const words = answerList
+        .flatMap((ans) => ans.answerText.toLowerCase().match(/\b[\w\d]+\b/g))
+        .reduce((acc, word) => {
+          acc[word] = (acc[word] || 0) + 1;
+          return acc;
+        }, {});
+
+      const wordCount = Object.entries(words).map(([text, value]) => ({
+        text: text,
+        value: value * 100,
       }));
-      console.log(categoryList);
-    }
-    const optionCount = categoryList.reduce((acc, item) => {
-      acc[item.optionId] = (acc[item.optionId] || 0) + 1;
-      return acc;
-    }, {});
+      console.log(wordCount);
+      setWordCloudData(wordCount);
+    } else {
+      const optionCount = categoryList.reduce((acc, item) => {
+        acc[item.optionId] = (acc[item.optionId] || 0) + 1;
+        return acc;
+      }, {});
 
-    const total = categoryList.length;
-    const chartData = Object.entries(optionCount)
-      .map(([option, count]) => ({
-        name: option,
-        value: count,
-        percentage: ((count / total) * 100).toFixed(2),
-      }))
-      .sort(function (a, b) {
-        return a.value - b.value;
+      const total = categoryList.length;
+      const chartData = Object.entries(optionCount)
+        .map(([option, count]) => ({
+          name: option,
+          value: count,
+          percentage: ((count / total) * 100).toFixed(2),
+        }))
+        .sort(function (a, b) {
+          return a.value - b.value;
+        });
+
+      const generatedColors = chartData.map((_, index) => {
+        const hue = (index * 360) / chartData.length;
+        return `hsl(${hue}, 50%, 60%)`;
       });
-
-    const generatedColors = chartData.map((_, index) => {
-      const hue = (index * 360) / chartData.length;
-      return `hsl(${hue}, 50%, 60%)`;
-    });
-    console.log(chartData);
-    console.log(generatedColors);
-    setPieChartData(chartData);
-    setPieChartColors(generatedColors);
+      console.log(chartData);
+      console.log(generatedColors);
+      setPieChartData(chartData);
+      setPieChartColors(generatedColors);
+    }
   }, [answerList]);
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -267,8 +252,9 @@ function AnswerQuestion() {
               Answers Received:
             </Typography>
           </Box>
-          {isOption && (
+          {isOption ? (
             <PieChart width={400} height={400}>
+              <Legend layout="horizontal" verticalAlign="top" align="center" />
               <Pie
                 data={pieChartData}
                 dataKey="value"
@@ -287,6 +273,10 @@ function AnswerQuestion() {
               <Tooltip />
               <Legend />
             </PieChart>
+          ) : (
+            <div>
+              <WordCloud data={wordCloudData} fontSize={(word) => word.value / 20} rotate={0} />
+            </div>
           )}
         </>
       )}
