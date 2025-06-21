e3f30932dff10ef9e78168a9faf0522d5eff8f41
Jake Vosloo <jake.vosloo@outlook.com>
2025-06-21 22:49:42 +1000

    cleanup redundant files

--- module/shared/ui/path.ts
+++
@@ -1,5 +0,0 @@
-import { route, index, layout } from "@react-router/dev/routes";
-
-export const publicRoutes = [index("./routes/HomePage.tsx"), route("login", "./routes/Login.tsx")];
-
-export const protectedRoutes = [route("logout", "./routes/Logout.tsx")];
