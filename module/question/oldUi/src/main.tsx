fe22fdbda1b6f70e32d8a6493928cf97e79be5df
Luke Yeh <lukeforwardto@gmail.com>
2025-06-21 20:35:08 +0800

    refactor: remove old ui

--- module/question/oldUi/src/main.tsx
+++
@@ -1,5 +0,0 @@
-import { renderApp } from "@zenmechat/shared-ui/main.tsx";
-import { AppRouter } from "@zenmechat/shared-ui/Router.tsx";
-import { publicRoutes, protectedRoutes } from "./Router";
-
-renderApp(<AppRouter publicRoutes={publicRoutes} protectedRoutes={protectedRoutes} />);
--- module/shared/oldUi/src/pages/HomePage.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "5rem" }}>
-      <h1>Welcome to the Portal</h1>
-      <p>This is the main entry of the application.</p>
-    </div>
-  );
-}
