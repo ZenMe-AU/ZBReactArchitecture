a14843b40237ef9c5a04889a1f54aef75aad3cba
Luke Yeh <lukeforwardto@gmail.com>
2025-07-09 15:11:31 +0800

    feat: add cmd and event tables

---
+++ module/questionV2/func/db/migration/20250707164211-create-event-table.js
@@ -0,0 +1,56 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("event", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      aggregateId: {
+        allowNull: true,
+        type: Sequelize.UUID,
+      },
+      aggregateType: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      causationId: {
+        allowNull: false,
+        type: Sequelize.UUID,
+      },
+      correlationId: {
+        allowNull: true,
+        type: Sequelize.UUID,
+      },
+      senderProfileId: {
+        allowNull: false,
+        type: Sequelize.UUID,
+      },
+      eventType: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      eventData: {
+        allowNull: false,
+        type: Sequelize.JSON,
+      },
+      originalData: {
+        allowNull: true,
+        type: Sequelize.JSON,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("event");
+  },
+};
