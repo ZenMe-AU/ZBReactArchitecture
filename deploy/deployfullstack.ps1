e0892fce7ca96796b3742619501aa16747e34ff0
Jake Vosloo <jake.vosloo@outlook.com>
2025-08-19 20:53:22 +1000

    deploy stack path change and comment

--- deploy/deployfullstack.ps1
+++ deploy/deployfullstack.ps1
@@ -1,11 +1,14 @@
+# This script runs the js files that run terraform files to deploy the environment and code.
+
 cd initEnv
 node ./initEnvironment.js
+
 cd ../deployEnv
 node ./deployEnvironment.js
-cd ..
-node ../ui/deploy/env/deployEnvironment.js
 
-#node ../module/questionV3/func/deploy/env/runTerraform.js
+cd ../../ui/deploy/env
+node ./deployEnvironment.js
+
 cd ../module/questionV3/func/deploy
 ./deployModule.ps1
 
