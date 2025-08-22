0dde550ae5d4bb1f617c6c921d8c9b31d408ef7b
Luke Yeh <lukeforwardto@gmail.com>
2025-08-23 02:05:30 +0800

    fix: prevent folder path overwrite error

--- module/questionV3/func/deploy/deployModule.ps1
+++ module/questionV3/func/deploy/deployModule.ps1
@@ -4,13 +4,13 @@
 # 3. Updating the database schema.
 # 4. Deploying the Function App code.
 
-# Set ROOT_FOLDER to one folder above the current directory
-$env:ROOT_FOLDER = Resolve-Path -Path ".."
-Write-Output "Set ROOT_FOLDER to $env:ROOT_FOLDER"
+# Set MODULE_FOLDER to one folder above the current directory
+$env:MODULE_FOLDER = Resolve-Path -Path ".."
+Write-Output "Set MODULE_FOLDER to $env:MODULE_FOLDER"
 
 # Deploy the environment (infrastructure for Function App)
 Write-Output "Deploying the Function App environment..."
-Set-Location $env:ROOT_FOLDER\deploy\env
+Set-Location $env:MODULE_FOLDER\deploy\env
 node ./deployEnvironment.js --auto-approve
 
 # Configure database security for Function App
@@ -19,10 +19,10 @@ node ./databaseSecurity.js
 
 # Update database schema for Function App
 Write-Output "Updating database schema..."
-Set-Location $env:ROOT_FOLDER\deploy\schema
+Set-Location $env:MODULE_FOLDER\deploy\schema
 node ./updateDbSchema.js
 
 # Deploy Function App code
 Write-Output "Deploying the Function App code..."
-Set-Location $env:ROOT_FOLDER\deploy\code
+Set-Location $env:MODULE_FOLDER\deploy\code
 node ./deploy.js
--- ui/deploy/deployUi.ps1
+++ ui/deploy/deployUi.ps1
@@ -3,15 +3,15 @@
 # 2. Deploying application code into the environment.
 
 # set a root folder environment variable to one folder above the current folder.
-$env:ROOT_FOLDER = Resolve-Path -Path ".."
-Write-Output "Set ROOT_FOLDER to $env:ROOT_FOLDER"
+$env:UI_FOLDER = Resolve-Path -Path ".."
+Write-Output "Set UI_FOLDER to $env:UI_FOLDER"
 
 #Deploy the ui environment(infra).
 Write-Output "Deploy the ui environment"
-Set-Location $env:ROOT_FOLDER\deploy\env
+Set-Location $env:UI_FOLDER\deploy\env
 node ./deployEnvironment.js --auto-approve
 
 #Deploy the application code
 Write-Output "Deploy the application code"
-Set-Location $env:ROOT_FOLDER\deploy\code
+Set-Location $env:UI_FOLDER\deploy\code
 node ./deploy.js
