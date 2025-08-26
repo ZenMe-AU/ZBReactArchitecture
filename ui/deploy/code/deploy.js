f96248c9a4163ecf7b8962707105c4710ebb3758
Luke Yeh <lukeforwardto@gmail.com>
2025-08-26 18:28:45 +0800

    fix: wrong function used in ui code deploy script

--- ui/deploy/code/deploy.js
+++ ui/deploy/code/deploy.js
@@ -3,20 +3,17 @@ import { getTargetEnv } from "../../../module/shared/func/deploy/util/envSetup.j
 import { execSync } from "child_process";
 import { fileURLToPath } from "url";
 import { dirname, resolve } from "path";
+import { getStorageAccountWebName } from "../../../module/shared/func/deploy/util/namingConvention.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
-function getStorageAccountName(targetEnv) {
-  return `${targetEnv}website`;
-}
 
 const moduleDir = resolve(__dirname, "..", "..");
 const distPath = resolve(moduleDir, "build", "client");
 
 function deploy() {
   const targetEnv = getTargetEnv();
-  const accountName = getStorageAccountName(targetEnv);
+  const accountName = getStorageAccountWebName(targetEnv);
 
   try {
     console.log("Building project...");
