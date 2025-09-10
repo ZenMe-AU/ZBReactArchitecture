faa10d7b359b5892de8d1ae548f3d88cf64b0d67
Luke Yeh <lukeforwardto@gmail.com>
2025-09-10 17:12:34 +0800

    refactor: move common service

--- module/quest3Tier/func/handler/handler.js
+++ module/quest3Tier/func/handler/handler.js
@@ -1,5 +1,5 @@
 const Question = require("../service/function.js");
-const { decode } = require("../shared/service/authUtils.js");
+const { decode } = require("../service/authUtils.js");
 const { followUpCmdQueue, shareQuestionCmdQueue } = require("../service/serviceBus.js");
 
 /**
