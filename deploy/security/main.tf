dbb02d6e7a55716a36635388545ebaf3281ba867
Wakenarey <rjaykitkat@gmail.com>
2025-09-15 18:38:22 +1000

    draft for private endpoint

--- deploy/security/main.tf
+++ deploy/security/main.tf
@@ -1,92 +1,3 @@
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       //version = "~> 4.0"
-#       version = "4.41.0"
-#     }
-#   }
-#   required_version = ">= 1.1.0"
-# }
-
-# provider "azurerm" {
-#   features {}
-#   subscription_id = "0930d9a7-2369-4a2d-a0b6-5805ef505868"
-# }
-
-# # Create a resource group for this environment
-# data "azurerm_resource_group" "rg" {
-#   name     = "ryanTest"
-# #  location = "australiaeast"
-# }
-
-# # Create a Front Door Standard/Premium profile
-# resource "azurerm_cdn_frontdoor_profile" "fd_profile" {
-#   name                = "ryanTestFrontDoorProfile"
-#   resource_group_name = data.azurerm_resource_group.rg.name
-#   sku_name            = "Standard_AzureFrontDoor"
-
-#   identity {
-#     type = "SystemAssigned"
-#   }
-# }
-
-# # Create a Front Door endpoint
-# resource "azurerm_cdn_frontdoor_endpoint" "fd_endpoint" {
-#   name                     = "ryantestfrontdoor"
-#   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.fd_profile.id
-# }
-
-# # Create an origin group (backend pool)
-# resource "azurerm_cdn_frontdoor_origin_group" "fd_origin_group" {
-#   name                = "default-origin-group"
-#   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.fd_profile.id
-
-#   session_affinity_enabled = false
-
-#   health_probe {
-#     interval_in_seconds = 30
-#     path                = "/"
-#     protocol            = "Https"
-#     request_type        = "HEAD"
-#   }
-
-#   load_balancing {
-#     sample_size                     = 4
-#     successful_samples_required      = 2
-#   }
-# }
-
-# # Create an origin (backend)
-# resource "azurerm_cdn_frontdoor_origin" "fd_origin" {
-#   name                          = "salmon-ground-staticapp"
-#   cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.fd_origin_group.id
-#   host_name                     = "salmon-ground-01f879d1e.5.azurestaticapps.net"
-#   enabled                       = true
-#   http_port                     = 80
-#   https_port                    = 443
-#   origin_host_header            = "salmon-ground-01f879d1e.5.azurestaticapps.net"
-#   certificate_name_check_enabled = true
-# }
-
-# data "azurerm_static_site" "swa" {
-#   name                = "ZenmeChatPOC1"
-#   resource_group_name = "ZenmeChatPOC1_group"
-# }
-
-# # Create a route (routing rule)
-# resource "azurerm_cdn_frontdoor_route" "fd_route" {
-#   name                              = "DefaultRoutingRule"
-#   cdn_frontdoor_endpoint_id         = azurerm_cdn_frontdoor_endpoint.fd_endpoint.id
-#   cdn_frontdoor_origin_ids          = [azurerm_cdn_frontdoor_origin.fd_origin.id]
-#   cdn_frontdoor_origin_group_id     = azurerm_cdn_frontdoor_origin_group.fd_origin_group.id
-#   patterns_to_match                 = ["/*"]
-#   supported_protocols               = ["Https"]
-#   forwarding_protocol               = "MatchRequest"
-#   // The 'https_redirect_enabled' field cannot be set to 'true' 
-#   // unless the 'supported_protocols' field contains both 'Http' and 'Https'
-#   https_redirect_enabled            = false
-# }
 
 #----------------------------------
 resource "azurerm_storage_account" "website" {
@@ -131,6 +42,13 @@ resource "azurerm_cdn_frontdoor_origin" "fd_origin" {
   https_port                    = 443
   origin_host_header = replace(replace(azurerm_storage_account.website.primary_web_endpoint, "https://", ""), "/", "")
   certificate_name_check_enabled = true
+
+  private_link {
+    request_message        = "Request access for Private Link Origin CDN Frontdoor"
+    target_type            = "Microsoft.Cdn/frontDoors/origins"
+    location               = data.azurerm_resource_group.main_resource.location
+    private_link_target_id = azurerm_private_endpoint.private_endpoint.id
+  }
 }
 
 resource "azurerm_cdn_frontdoor_origin_group" "fd_origin_group" {
@@ -206,5 +124,32 @@ resource "azurerm_dns_cname_record" "cname_record" {
   resource_group_name = "root-zenblox"
   ttl                 = 3600
   record              = azurerm_cdn_frontdoor_endpoint.fd_endpoint.host_name
+}
+
+resource "azurerm_private_endpoint" "private_endpoint" {
+  name                = "${var.storage_account_web_name}-privendpoint"
+  location            = data.azurerm_resource_group.main_resource.location
+  resource_group_name = data.azurerm_resource_group.main_resource.name
+  subnet_id           = azurerm_subnet.private_endpoint_subnet.id
+
+  private_service_connection {
+    name                           = "${var.storage_account_web_name}-privateserviceconnection"
+    private_connection_resource_id = azurerm_cdn_frontdoor_origin.fd_origin.id
+    is_manual_connection           = false
+  }
+}
+
+resource "azurerm_virtual_network" "virtual_network" {
+  name                = "${var.storage_account_web_name}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = data.azurerm_resource_group.main_resource.location
+  resource_group_name = data.azurerm_resource_group.main_resource.name
+}
+
+resource "azurerm_subnet" "private_endpoint_subnet" {
+  name                 = "${var.storage_account_web_name}-privatesubnet"
+  resource_group_name  = data.azurerm_resource_group.main_resource.name
+  virtual_network_name = azurerm_virtual_network.virtual_network.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
