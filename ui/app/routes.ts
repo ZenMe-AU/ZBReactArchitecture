f0303b9f6c9585a9560e0a0fbb630b65b6bb910c
Luke Yeh <lukeforwardto@gmail.com>
2025-09-17 23:26:20 +0800

    remove questionV2

--- ui/app/routes.ts
+++ ui/app/routes.ts
@@ -1,7 +1,6 @@
 import type { RouteConfig } from "@react-router/dev/routes";
 import { route, index, layout } from "@react-router/dev/routes";
 import { protectedRoutes as questionProtectedRoutes, publicRoutes as questionPublicRoutes } from "../../module/question/ui/routes";
-import { protectedRoutes as questionV2ProtectedRoutes, publicRoutes as questionV2PublicRoutes } from "../../module/questionV2/ui/routes";
 import { protectedRoutes as quest5TierProtectedRoutes, publicRoutes as quest5TierPublicRoutes } from "../../module/quest5Tier/ui/routes";
 import { protectedRoutes as profileProtectedRoutes, publicRoutes as profilePublicRoutes } from "../../module/profile/ui/routes";
 // console.log("Question Routes:", questionRoutes);
@@ -21,13 +20,11 @@ export default [
   route("login", "./routes/Login.tsx"),
   ...questionPublicRoutes,
   ...profilePublicRoutes,
-  ...questionV2PublicRoutes,
   ...quest5TierPublicRoutes,
   layout("./layouts/protected.tsx", [
     route("logout", "./routes/Logout.tsx"),
     ...questionProtectedRoutes,
     ...profileProtectedRoutes,
-    ...questionV2ProtectedRoutes,
     ...quest5TierProtectedRoutes,
   ]),
 ] satisfies RouteConfig;
