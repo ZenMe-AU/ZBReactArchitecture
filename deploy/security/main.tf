f6c93fccc510c85e25ce13563c9195837778374e
Wakenarey <rjaykitkat@gmail.com>
2025-09-23 17:00:05 +1000

    Commented out Premium AzureFrontDoor private endpoint version, switching to Standard version.

--- deploy/security/main.tf
+++ deploy/security/main.tf
@@ -1,5 +1,20 @@
 
 #----------------------------------
+resource "azurerm_virtual_network" "virtual_network" {
+  name                = "${var.storage_account_web_name}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = data.azurerm_resource_group.main_resource.location
+  resource_group_name = data.azurerm_resource_group.main_resource.name
+}
+resource "azurerm_subnet" "private_endpoint_subnet" {
+  name                 = "${var.storage_account_web_name}-privatesubnet"
+  resource_group_name  = data.azurerm_resource_group.main_resource.name
+  virtual_network_name = azurerm_virtual_network.virtual_network.name
+  address_prefixes     = ["10.0.1.0/24"]
+  # Required so the storage account network_rules can reference this subnet
+  # for network ACLs; without this Azure returns SubnetsHaveNoServiceEndpointsConfigured
+  # service_endpoints    = ["Microsoft.Storage"]
+}
 resource "azurerm_storage_account" "website" {
   name                     = var.storage_account_web_name
   resource_group_name      = data.azurerm_resource_group.main_resource.name
@@ -7,42 +22,39 @@ resource "azurerm_storage_account" "website" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
   # Restrict network access to the storage account
-  network_rules {
-    default_action             = "Deny" # Deny all access by default
-    bypass                     = ["AzureServices"] # Allow trusted Azure services to bypass network rules
-    virtual_network_subnet_ids = [azurerm_subnet.private_endpoint_subnet.id] # Only allow access from this specific subnet
-
-  }
+  # network_rules {
+  #   default_action             = "Deny" # Deny all access by default
+  # }
 }
 
 # Create a storage container for static web content
 resource "azurerm_storage_container" "static_content" {
   name                  = "$web"
   storage_account_id    = azurerm_storage_account.website.id
-  container_access_type = "blob"
-  
+  container_access_type = "blob"  
 }
 
 resource "azurerm_storage_account_static_website" "website" {
   storage_account_id = azurerm_storage_account.website.id
   index_document     = "index.html"
-  error_404_document = "index.html"
-  
+  error_404_document = "index.html"  
 }
+
+# using Front Door Standard/Premium Private Link origin instead.
 resource "azurerm_cdn_frontdoor_profile" "fd_profile" {
   name                = "${var.storage_account_web_name}-fd-profile"   
   resource_group_name = data.azurerm_resource_group.main_resource.name  
-  sku_name            = "Standard_AzureFrontDoor"  
-                     
+  # sku_name            = "Premium_AzureFrontDoor"
+  sku_name            = "Standard_AzureFrontDoor"                       
+  identity {
+    type = "SystemAssigned"
+  }
 }
-
-
 
 resource "azurerm_cdn_frontdoor_endpoint" "fd_endpoint" {
   name                     = "${var.storage_account_web_name}-fd-endpoint"
   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.fd_profile.id  
 }
-
 
 resource "azurerm_cdn_frontdoor_origin_group" "fd_origin_group" {
   name                = "${var.storage_account_web_name}-fd-origin"
@@ -62,7 +74,54 @@ resource "azurerm_cdn_frontdoor_origin_group" "fd_origin_group" {
     successful_samples_required      = 2
   }
 }
+# creating domain name
+resource "azurerm_cdn_frontdoor_custom_domain" "custom_domain" {
+  name                        = "${var.target_env}-dns"
+  cdn_frontdoor_profile_id    = azurerm_cdn_frontdoor_profile.fd_profile.id
+  host_name                   = lower("${var.target_env}.zenblox.com.au")
+  tls {
+    certificate_type = "ManagedCertificate"
+    }
+}
+#creates dns validation:
+resource "azurerm_dns_txt_record" "dns_validation" {
+  name                = "_dnsauth.${var.target_env}"
+  zone_name           = "zenblox.com.au"
+  resource_group_name = "root-zenblox"
+  ttl                 = 3600
+  record {
+    value = azurerm_cdn_frontdoor_custom_domain.custom_domain.validation_token
+  }
+}
 
+# creates cname record to point to front door endpoint
+resource "azurerm_dns_cname_record" "cname_record" {
+  name                = var.target_env
+  zone_name           = "zenblox.com.au"
+  resource_group_name = "root-zenblox"
+  ttl                 = 3600
+  record              = azurerm_cdn_frontdoor_endpoint.fd_endpoint.host_name
+}
+
+resource "azurerm_cdn_frontdoor_origin" "fd_origin" {
+  name                          = "${var.storage_account_web_name}-fd-origin"
+  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.fd_origin_group.id
+  # Use the blob endpoint host (excludes https://)
+  host_name                     = azurerm_storage_account.website.primary_blob_host
+  enabled                       = true
+  http_port                     = 80
+  https_port                    = 443
+  origin_host_header            = azurerm_storage_account.website.primary_blob_host
+  certificate_name_check_enabled = true
+
+  # Configure Private Link directly on the Front Door origin to storage account blob subresource
+  # private_link {
+  #   request_message        = "Approve Front Door access to blob origin"
+  #   target_type            = "blob"
+  #   location               = data.azurerm_resource_group.main_resource.location
+  #   private_link_target_id = azurerm_storage_account.website.id
+  # }
+}
 resource "azurerm_cdn_frontdoor_route" "fd_route" {
   name                              = "${var.storage_account_web_name}-fd-route"
   cdn_frontdoor_endpoint_id         = azurerm_cdn_frontdoor_endpoint.fd_endpoint.id
@@ -87,75 +146,12 @@ resource "azurerm_cdn_frontdoor_route" "fd_route" {
   ]
 }
 
-#creates dns validation:
-resource "azurerm_dns_txt_record" "dns_validation" {
-  name                = "_dnsauth.${var.target_env}"
-  zone_name           = "zenblox.com.au"
-  resource_group_name = "root-zenblox"
-  ttl                 = 3600
 
-  record {
-    value = azurerm_cdn_frontdoor_custom_domain.custom_domain.validation_token
-  }
-}
-# creates cname record to point to front door endpoint
-resource "azurerm_dns_cname_record" "cname_record" {
-  name                = var.target_env
-  zone_name           = "zenblox.com.au"
-  resource_group_name = "root-zenblox"
-  ttl                 = 3600
-  record              = azurerm_cdn_frontdoor_endpoint.fd_endpoint.host_name
-}
 
-# creating domain name
-resource "azurerm_cdn_frontdoor_custom_domain" "custom_domain" {
-  name                        = "${var.target_env}-dns"
-  cdn_frontdoor_profile_id    = azurerm_cdn_frontdoor_profile.fd_profile.id
-  host_name                   = "${var.target_env}.zenblox.com.au"
-  tls {
-    certificate_type = "ManagedCertificate"
-    
-  }
-}
-resource "azurerm_virtual_network" "virtual_network" {
-  name                = "${var.storage_account_web_name}-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = data.azurerm_resource_group.main_resource.location
-  resource_group_name = data.azurerm_resource_group.main_resource.name
-}
 
-resource "azurerm_subnet" "private_endpoint_subnet" {
-  name                 = "${var.storage_account_web_name}-privatesubnet"
-  resource_group_name  = data.azurerm_resource_group.main_resource.name
-  virtual_network_name = azurerm_virtual_network.virtual_network.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-resource "azurerm_private_endpoint" "private_endpoint" {
-  name                = "${var.storage_account_web_name}-privendpoint"
-  location            = data.azurerm_resource_group.main_resource.location
-  resource_group_name = data.azurerm_resource_group.main_resource.name
-  subnet_id           = azurerm_subnet.private_endpoint_subnet.id
 
-  private_service_connection {
-    name                           = "${var.storage_account_web_name}-privateserviceconnection"
-    private_connection_resource_id = azurerm_storage_account_static_website.website.id
-    is_manual_connection           = false
-  }
-}
-resource "azurerm_cdn_frontdoor_origin" "fd_origin" {
-  name                          = "${var.storage_account_web_name}-fd-origin"
-  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.fd_origin_group.id
-  host_name = replace(replace(azurerm_storage_account.website.primary_web_endpoint, "https://", ""), "/", "")
-  enabled                       = true
-  http_port                     = 80
-  https_port                    = 443
-  origin_host_header = replace(replace(azurerm_storage_account.website.primary_web_endpoint, "https://", ""), "/", "")
-  certificate_name_check_enabled = true
 
-  private_link {
-    request_message        = "Request access for Private Link Origin CDN Frontdoor"
-    target_type            = "Microsoft.Cdn/frontDoors/origins"
-    location               = data.azurerm_resource_group.main_resource.location
-    private_link_target_id = azurerm_private_endpoint.private_endpoint.id
-  }
-}
+
+
+
+
