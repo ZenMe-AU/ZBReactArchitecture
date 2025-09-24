384984d2d9292e6f29439a9be3f187c359912bce
Wakenarey <rjaykitkat@gmail.com>
2025-09-24 16:04:48 +1000

    editied code so that it used standard front door and made it so urls that lead to the web app redirected to the front door url

--- deploy/security/main.tf
+++ deploy/security/main.tf
@@ -31,7 +31,11 @@ resource "azurerm_storage_account" "website" {
 resource "azurerm_storage_container" "static_content" {
   name                  = "$web"
   storage_account_id    = azurerm_storage_account.website.id
-  container_access_type = "blob"  
+  # Make the $web container private so direct blob URLs like
+  # https://<account>.blob.core.windows.net/$web/index.html are blocked.
+  # Front Door uses the static website endpoint (web.core.windows.net),
+  # which continues to work regardless of this ACL.
+  container_access_type = "private"  
 }
 
 resource "azurerm_storage_account_static_website" "website" {
@@ -64,9 +68,9 @@ resource "azurerm_cdn_frontdoor_origin_group" "fd_origin_group" {
 
   health_probe {
     interval_in_seconds = 30
-    path                = "/"
+    path                = "/index.html"
     protocol            = "Https"
-    request_type        = "HEAD"
+    request_type        = "GET"
   }
 
   load_balancing {
@@ -94,6 +98,38 @@ resource "azurerm_dns_txt_record" "dns_validation" {
   }
 }
 
+# Rules Engine: force custom domain by redirecting requests whose Host != custom domain
+resource "azurerm_cdn_frontdoor_rule_set" "fd_rules" {
+  name                      = "${var.storage_account_web_name}ruleset"
+  cdn_frontdoor_profile_id  = azurerm_cdn_frontdoor_profile.fd_profile.id
+}
+
+resource "azurerm_cdn_frontdoor_rule" "enforce_custom_host" {
+  name                        = "${var.storage_account_web_name}EnforceCustomHost"
+  cdn_frontdoor_rule_set_id   = azurerm_cdn_frontdoor_rule_set.fd_rules.id
+  order                       = 1
+
+  conditions {
+    host_name_condition {
+      operator          = "Equal"
+      match_values      = [lower("${var.target_env}.zenblox.com.au")]
+      negate_condition  = true
+      transforms        = []
+    }
+  }
+
+  actions {
+    url_redirect_action {
+      redirect_type      = "PermanentRedirect"  # 308
+      destination_hostname = lower("${var.target_env}.zenblox.com.au")
+      destination_path   = "/{path}"
+      destination_fragment = "{fragment}"
+      query_string       = "{query_string}"
+      redirect_protocol  = "Https"
+    }
+  }
+}
+
 # creates cname record to point to front door endpoint
 resource "azurerm_dns_cname_record" "cname_record" {
   name                = var.target_env
@@ -106,12 +142,12 @@ resource "azurerm_dns_cname_record" "cname_record" {
 resource "azurerm_cdn_frontdoor_origin" "fd_origin" {
   name                          = "${var.storage_account_web_name}-fd-origin"
   cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.fd_origin_group.id
-  # Use the blob endpoint host (excludes https://)
-  host_name                     = azurerm_storage_account.website.primary_blob_host
+  # Lowest-cost: point to the Storage Static Website public endpoint
+  host_name                     = azurerm_storage_account.website.primary_web_host
   enabled                       = true
   http_port                     = 80
   https_port                    = 443
-  origin_host_header            = azurerm_storage_account.website.primary_blob_host
+  origin_host_header            = azurerm_storage_account.website.primary_web_host
   certificate_name_check_enabled = true
 
   # Configure Private Link directly on the Front Door origin to storage account blob subresource
@@ -127,9 +163,11 @@ resource "azurerm_cdn_frontdoor_route" "fd_route" {
   cdn_frontdoor_endpoint_id         = azurerm_cdn_frontdoor_endpoint.fd_endpoint.id
   cdn_frontdoor_origin_ids          = [azurerm_cdn_frontdoor_origin.fd_origin.id]
   cdn_frontdoor_origin_group_id     = azurerm_cdn_frontdoor_origin_group.fd_origin_group.id
+  cdn_frontdoor_rule_set_ids        = [azurerm_cdn_frontdoor_rule_set.fd_rules.id]
   patterns_to_match                 = ["/*"]
   supported_protocols               = ["Http", "Https"]
   forwarding_protocol               = "HttpsOnly"
+  link_to_default_domain            = true
   // The 'https_redirect_enabled' field cannot be set to 'true' 
   // unless the 'supported_protocols' field contains both 'Http' and 'Https'
   https_redirect_enabled            = true
