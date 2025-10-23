64db1b58931ad9062ea56e7a92fe1dbf0aad428b
Jake Vosloo <jake.vosloo@outlook.com>
2025-10-24 08:22:07 +1000

    ESlint initial config

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+// From Eslint v8.21.0, .eslintrc* is no longer used. eslint.config.js is the default config file name.
+import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  {
+    files: ["**/*.{ts,tsx, mts, cts}", "**/*.{js,jsx,mjs,cjs}"],
+    plugins: {
+      js,
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+    },
+  },
+];
