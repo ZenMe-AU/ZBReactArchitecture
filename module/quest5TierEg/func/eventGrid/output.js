9e14a21a39978081404d237dfcc89842a8c1bb6a
Luke Yeh <lukeforwardto@gmail.com>
2025-10-27 01:03:18 +0800

    chore: remove redundant event grid files

--- module/quest5TierEg/func/eventGrid/output.js
+++
@@ -1,9 +0,0 @@
-const { output } = require("@azure/functions");
-
-const eventGridDomain = output.eventGrid({
-  connection: "EventGridConnection",
-});
-
-module.exports = {
-  eventGridDomain,
-};
