ba4e3cdaccc02d7e54bd48eb630b1ec8758eff1f
Luke Yeh <lukeforwardto@gmail.com>
2025-11-12 21:07:44 +0800

    fix: profile DB_TYPE path

--- module/profile/func/deploy/db/initData.js
+++ module/profile/func/deploy/db/initData.js
@@ -8,7 +8,7 @@ const { resolve } = require("path");
 const classRunMigration = require("./classRunMigration.js");
 const { getTargetEnv, getModuleName } = require("../util/envSetup.js");
 const { createDatabaseInstance } = require("../../repository/model/connection/index.js");
-const DB_TYPE = require("../enum/dbType");
+const DB_TYPE = require("../../enum/dbType.js");
 const { getDbAdminName, getPgHost } = require("../util/namingConvention.js");
 
 const moduleDir = resolve(__dirname, "..", "..", "..");
