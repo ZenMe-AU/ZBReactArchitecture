f27716d366321d16a14b2cff37318f3690c17986
Luke Yeh <lukeforwardto@gmail.com>
2025-11-13 19:26:43 +0800

    set cwd to lint-staged and update config

---
+++ .codingstandards/checkLockfile.mjs
@@ -0,0 +1,14 @@
+/**
+ * @license SPDX-FileCopyrightText: © 2025 Zenme Pty Ltd <info@zenme.com.au>
+ * @license SPDX-License-Identifier: MIT
+ */
+
+import { execSync } from "child_process";
+
+try {
+  execSync("pnpm install --frozen-lockfile", { stdio: "inherit" });
+  //   throw new Error("something went wrong");
+} catch {
+  console.error(" Lockfile out of sync. Run pnpm install and commit again.");
+  process.exit(1);
+}
--- .codingstandards/lint-staged.config.mjs
+++ .codingstandards/lint-staged.config.mjs
@@ -1,3 +1,13 @@
+/**
+ * @license SPDX-FileCopyrightText: © 2025 Zenme Pty Ltd <info@zenme.com.au>
+ * @license SPDX-License-Identifier: MIT
+ */
+
 export default {
-  "*.{js,jsx,ts,tsx,cjs}": ["eslint --fix", "prettier --write"],
+  // "../package.json": ["pnpm install --frozen-lockfile || (echo '❌ Lockfile out of sync. Run pnpm install and commit again.' && exit 1)"],
+  // "../package.json": ["prettier --write"],
+  // "../package.json": ["pnpm install --frozen-lockfile", "exit 2"],
+  "{package.json,pnpm-lock.yaml}": ["node .codingstandards/checkLockfile.mjs"],
+  "*.{js,jsx,ts,tsx}": ["pnpm exec prettier --write"],
+  // "*.{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write"],
 };
