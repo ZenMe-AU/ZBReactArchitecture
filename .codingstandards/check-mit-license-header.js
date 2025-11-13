790c1b0092d30bfc40cbe16fb256f6ad9921bf1a
Jake Vosloo <jake.vosloo@outlook.com>
2025-11-13 22:06:30 +1000

    test precommit hook

--- .codingstandards/check-mit-license-header.js
+++ .codingstandards/check-mit-license-header.js
@@ -3,7 +3,7 @@
  * @license SPDX-License-Identifier: MIT
  */
 
-// check-mit-license-header.js test
+// check-mit-license-header.js test2
 const fs = require("fs");
 const { execSync } = require("child_process");
 const path = require("path");
