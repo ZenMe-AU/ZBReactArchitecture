e07a123e285ed7725af8c0936cc34979f449ee1b
Luke Yeh <lukeforwardto@gmail.com>
2025-12-31 15:51:12 +0800

    add runner test

--- runner/start.sh
+++ runner/start.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 set -e
 
+if [[ -z "$RUNNER_TOKEN" ]]; then
+  echo "RUNNER_TOKEN is not set"
+  exit 1
+fi
+
+# Org runner or Repo runner
+# if [[ -n "$GITHUB_REPO" ]]; then
+#   RUNNER_URL="https://github.com/${GITHUB_ORG}/${GITHUB_REPO}"
+# else
+#   RUNNER_URL="https://github.com/${GITHUB_ORG}"
+# fi
+RUNNER_URL="https://github.com/ZenMe-AU/ZBReactArchitecture" #TODO:use env var
+
+RUNNER_NAME="${RUNNER_NAME:-aca-runner}-$(hostname)"
+
 ./config.sh \
-  --url https://github.com/<org-or-repo> \
+  --url "$RUNNER_URL" \
   --token "$RUNNER_TOKEN" \
+  --name "$RUNNER_NAME" \
+  --labels "aca,ephemeral" \
   --unattended \
+  --ephemeral \
   --replace
 
 ./run.sh
