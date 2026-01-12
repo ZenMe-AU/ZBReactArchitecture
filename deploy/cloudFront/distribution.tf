a4fc2c9617208c089af274a35ced61d26e56f861
Jake Vosloo <jpvosloo@users.noreply.github.com>
2026-01-12 17:57:57 +1000

    example code for ACM certificate and Route53 validation for CloudFront
    Signed-off-by: Jake Vosloo <jpvosloo@users.noreply.github.com>

--- deploy/cloudFront/distribution.tf
+++ deploy/cloudFront/distribution.tf
@@ -115,3 +115,47 @@ resource "aws_cloudfront_distribution" "primary" {
     ssl_support_method             = "sni-only"
   }
 }
+
+#################################
+resource "aws_acm_certificate" "cert" {
+  domain_name       = "*.z3nm3.com.au"       # Use your distribution domain name
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "aws_route53_record" "acm_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      type   = dvo.resource_record_type
+      value  = dvo.resource_record_value
+    }
+  }
+
+  zone_id = "<HOSTED_ZONE_ID>"            # Replace with your Route53 Zone ID
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 300
+  records = [each.value.value]
+}
+
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.acm_validation : record.fqdn]
+}
+
+resource "aws_cloudfront_distribution" "primary" {
+  # ... other config ...
+
+  viewer_certificate {
+    acm_certificate_arn            = aws_acm_certificate.cert.arn
+    cloudfront_default_certificate = false
+    ssl_support_method             = "sni-only"
+    minimum_protocol_version       = "TLSv1.3_2025"
+  }
+
+  # ... rest of config ...
+}
+
