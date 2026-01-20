b662209964b3d8de8a9760108c8cc9193adfd317
Luke Yeh <lukeforwardto@gmail.com>
2026-01-20 21:15:48 +0800

    update msal spa redirect uri

--- deploy/corpSetup/c11cloudfront/source/msalSpa/src/authConfig.ts
+++ deploy/corpSetup/c11cloudfront/source/msalSpa/src/authConfig.ts
@@ -5,8 +5,8 @@ export const msalConfig: Configuration = {
   auth: {
     clientId: "b0ee1412-c79a-48f3-978e-f61f740d7832",
     authority: "https://login.microsoftonline.com/15fb0613-7977-4551-801b-6aadac824241", //use common for multi-tenant app
-    // redirectUri: "https://logintest.zenblox.com.au", // Must match Azure App Registration
-    redirectUri: "http://localhost:3000", // Must match Azure App Registration
+    redirectUri: "https://login.zenblox.com.au", // Must match Azure App Registration
+    // redirectUri: "http://localhost:3000", // Must match Azure App Registration
   },
   cache: {
     cacheLocation: "sessionStorage", // easier for local dev
