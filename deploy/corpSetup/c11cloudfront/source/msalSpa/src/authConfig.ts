0c1313b88771940127541ea7ab1e9f484738a7a7
Luke Yeh <lukeforwardto@gmail.com>
2026-01-25 15:42:11 +0800

    switch login spa config to runtime-injected config.js

--- deploy/corpSetup/c11cloudfront/source/msalSpa/src/authConfig.ts
+++ deploy/corpSetup/c11cloudfront/source/msalSpa/src/authConfig.ts
@@ -1,12 +1,32 @@
 import { type Configuration } from "@azure/msal-browser";
+const cfg =
+  window.__APP_CONFIG__ ||
+  (() => {
+    if (import.meta.env.MODE === "development") {
+      // dev fallback
+      return {
+        clientId: "b0ee1412-c79a-48f3-978e-f61f740d7832",
+        redirectUri: "http://localhost:3000",
+      };
+    }
+    // production fallback
+    return null;
+  })();
+
+if (!cfg) {
+  throw new Error("config.js not loaded");
+}
+
+console.log("Local config:", cfg);
 
 // MSAL configuration
 export const msalConfig: Configuration = {
   auth: {
-    clientId: "b0ee1412-c79a-48f3-978e-f61f740d7832",
+    // clientId: "b0ee1412-c79a-48f3-978e-f61f740d7832",
+    clientId: cfg.clientId,
     authority: "https://login.microsoftonline.com/15fb0613-7977-4551-801b-6aadac824241", //use common for multi-tenant app
-    redirectUri: "https://login.zenblox.com.au", // Must match Azure App Registration
-    // redirectUri: "http://localhost:3000", // Must match Azure App Registration
+    // redirectUri: "https://login.zenblox.com.au", // Must match Azure App Registration
+    redirectUri: cfg.redirectUri,
   },
   cache: {
     cacheLocation: "sessionStorage", // easier for local dev
