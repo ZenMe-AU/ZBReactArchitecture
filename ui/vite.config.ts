367af5d55ec79170a6b331f07ca63112e6a9897b
henryhan <henryhan@gliacloud.com>
2026-03-08 21:02:49 +0800

    feat: set up module federation

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -5,7 +5,28 @@
 
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
+import federation from "@originjs/vite-plugin-federation";
 
-export default defineConfig({
-  plugins: [reactRouter()],
+export default defineConfig(({ isSsrBuild }) => {
+  const mfPlugin = federation({
+    name: "uiHost",
+    remotes: {
+      quest3TierRemote: process.env.VITE_QUEST3TIER_REMOTE_URL ?? "http://localhost:5174/assets/remoteEntry.js",
+      quest5TierRemote: process.env.VITE_QUEST5TIER_REMOTE_URL ?? "http://localhost:5175/assets/remoteEntry.js",
+      quest5TierEgRemote: process.env.VITE_QUEST5TIEREG_REMOTE_URL ?? "http://localhost:5176/assets/remoteEntry.js",
+    },
+    shared: ["react", "react-dom", "react-router", "react-router-dom"],
+  });
+  const plugins = isSsrBuild ? [reactRouter()] : [mfPlugin, reactRouter()];
+
+  return {
+    plugins,
+    build: {
+      target: "esnext",
+    },
+    server: {
+      port: 5173,
+      strictPort: true,
+    },
+  };
 });
