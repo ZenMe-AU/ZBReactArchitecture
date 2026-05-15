ee835d757eb8ac6268e32becf303ae38576676c6
Jake Vosloo <jpvosloo@users.noreply.github.com>
2026-05-15 16:37:36 +1000

    feat(database): add script for local database schema updates

---
+++ module/quest3Tier/func/deploy/db/updateDbSchemaLocal.js
@@ -0,0 +1,34 @@
+/**
+ * @license SPDX-FileCopyrightText: © 2026 Zenme Pty Ltd <info@zenme.com.au>
+ * @license SPDX-License-Identifier: MIT
+ */
+
+const { resolve } = require("path");
+const classRunMigration = require("./classRunMigration.js");
+const { getTargetEnv, getModuleName } = require("../../../../../deploy/util/envSetup.cjs");
+const { createDatabaseInstance } = require("../../repository/model/connection/index.js");
+const DB_TYPE = require("../../enum/dbType.js");
+const { getDbAdminName, getPgHost } = require("../../../../../deploy/util/namingConvention.cjs");
+
+const moduleDir = resolve(__dirname, "..", "..", "..");
+const migrationDir = resolve(__dirname, "migration");
+
+(async () => {
+  const envType = process.env.TF_VAR_env_type || "dev";
+  const targetEnv = getTargetEnv();
+  const moduleName = getModuleName(moduleDir);
+
+  const pgAdminUserName = process.env.TF_VAR_deployer_sp_name || getDbAdminName(envType); //"getDbSchemaAdminName(moduleName)";
+  const db = await createDatabaseInstance(DB_TYPE.POSTGRES, {
+    username: "vscode", //pgAdminUserName,
+    password: "vscode",
+    authMode: "password",
+    host: "127.0.0.1", //getPgHost(targetEnv),
+    dialect: "postgres",
+    port: 5432,
+    database: moduleName,
+    logging: false,
+  });
+  const direction = process.argv[2] || "up";
+  await new classRunMigration({ db, migrationDir, envType, targetEnv }).run(direction);
+})();
