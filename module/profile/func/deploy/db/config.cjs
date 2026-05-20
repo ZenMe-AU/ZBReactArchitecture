be78f29f9514e49814d3169ebc53725b9e7469a8
Luke Yeh <175194915+luke-yeh@users.noreply.github.com>
2026-05-20 19:58:47 +0800

    fix(db config): update database name to 'profile' for local and test environments

--- module/profile/func/deploy/db/config.cjs
+++ module/profile/func/deploy/db/config.cjs
@@ -9,14 +9,14 @@ module.exports = {
   local: {
     username: "vscode",
     password: "vscode",
-    database: "quest3Tier",
+    database: "profile",
     host: "127.0.0.1",
     dialect: "postgres",
   },
   test: {
     username: "root",
     password: null,
-    database: "quest3Tier",
+    database: "profile",
     host: "127.0.0.1",
     dialect: "postgres",
   },
