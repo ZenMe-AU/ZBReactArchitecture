1b286002cb30dd6a26ffd589c7ab64d5a9a3dbcb
henryhan <henryhan@gliacloud.com>
2026-07-03 11:46:48 +0800

    chore: update clientId in msalConfig

--- ui/auth/msalInstance.ts
+++ ui/auth/msalInstance.ts
@@ -8,7 +8,7 @@ import { PublicClientApplication } from "@azure/msal-browser";
 // import { msalConfig } from "./authConfig";
 const msalConfig = {
   auth: {
-    clientId: "d70d018d-1452-4958-8dc7-70dd30f59e30",
+    clientId: "83d65e94-41f8-460a-943a-edee7d9e096b",
     authority: `https://login.microsoftonline.com/15fb0613-7977-4551-801b-6aadac824241`, //use common for multi-tenant app
     redirectUri: import.meta.env.MODE === "development" ? "http://localhost:5173" : "https://www.prod.z3nm3.com/login",
   },
