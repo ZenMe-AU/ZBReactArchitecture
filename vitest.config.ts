0632464f409091066517aba587d28a46a0a6e833
Jake Vosloo <jpvosloo@users.noreply.github.com>
2026-07-08 20:27:21 +1000

    vitest config in root folder

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,26 +2,9 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    projects: [
-      {
-        test: {
-          name: "web",
-          include: ["src/**/*.{test,spec}.{ts,tsx,js,jsx}"],
-          environment: "jsdom",
-          globals: true,
-          passWithNoTests: true,
-        },
-      },
-      {
-        test: {
-          name: "functions",
-          include: ["app/src/**/*.{test,spec}.{ts,tsx,js,jsx}"],
-          environment: "node",
-          globals: true,
-          passWithNoTests: true,
-        },
-      },
-    ],
-    passWithNoTests: true,
+    globals: true,
+    environment: "node",
+    fileParallelism: false,
+    projects: ["module/*/func/"],
   },
 });
